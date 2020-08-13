8468faf4fe7592525d88682816174bfed497f354
Erick Alves <e7alves@gmail.com>
2020-08-12 21:26:28 -0300

    Paragraphs extraction

--- src/content.js
+++ src/content.js
@@ -3,10 +3,15 @@
   const titleElem = headElem.querySelector('title')
   const descriptionMetaElem = headElem.querySelector('[name=description]')
   const bodyElem = document.querySelector('body')
+  const paragraphElems = bodyElem ? bodyElem.querySelectorAll('p') : []
+  const content = []
+  paragraphElems.forEach(elem => {
+    content.push(elem.innerText)
+  })
   chrome.runtime.sendMessage({
     eventName: 'page-loaded',
     title: titleElem && titleElem.innerText,
     description: descriptionMetaElem && descriptionMetaElem.content,
-    content: bodyElem && bodyElem.innerText,
+    content,
   })
 })()
