1bab5715d8f9c392fb2837460531871c7311cf99
Erick Alves <e7alves@gmail.com>
2020-08-18 21:50:07 -0300

    Fitting words mapping

--- __tests__/wordsMapping.test.js
+++ __tests__/wordsMapping.test.js
@@ -5,23 +5,28 @@ test('Returns correct words map', () => {
     ['do', 'you', 'know', 'what', 'it', 'is'],
     ['excuse', 'me', 'do', 'you', 'know', 'what', 'time', 'it', 'is'],
   ]
-  const wordsMapping = new WordsMapping()
-  const expected = ['do', 'you', 'know', 'what', 'it', 'is', 'excuse', 'me', 'time']
-  expect(wordsMapping.generateMap(sentences)).toEqual(expected)
+  const keyWords = ['about', 'thing', 'time']
+  const wordsMapping = new WordsMapping(sentences, keyWords)
+  const expected = ['do', 'you', 'know', 'what', 'it', 'is', 'excuse', 'me', 'time', 'about', 'thing']
+  expect(wordsMapping.generateMap(sentences, keyWords)).toEqual(expected)
 })
 
 test('Given a words map returns correct sentences map', () => {
-  const wordsMap = ['do', 'excuse', 'is', 'it', 'know', 'me', 'time', 'what', 'you']
+  const wordsMap = ['about', 'do', 'excuse', 'is', 'it', 'know', 'me', 'thing', 'time', 'what', 'you']
   const sentences = [
     ['do', 'you', 'know', 'what', 'it', 'is'],
     ['excuse', 'me', 'do', 'you', 'know', 'what', 'time', 'it', 'is'],
   ]
-  const wordsMapping = new WordsMapping()
-  const expected = [
-    [0, 8, 4, 7, 3, 2],
-    [1, 5, 0, 8, 4, 7, 6, 3, 2],
-  ]
-  expect(wordsMapping.generateMappedSentences(sentences, wordsMap)).toEqual(expected)
+  const keyWords = ['about', 'thing', 'time']
+  const wordsMapping = new WordsMapping(sentences, keyWords)
+  const expected = {
+    sentences: [
+      [1, 10, 5, 9, 4, 3],
+      [2, 6, 1, 10, 5, 9, 8, 4, 3],
+    ],
+    keyWords: [0, 7, 8],
+  }
+  expect(wordsMapping.generateMappedSentences(wordsMap)).toEqual(expected)
 })
 
 test('Returns correct sentences map', () => {
@@ -29,21 +34,25 @@ test('Returns correct sentences map', () => {
     ['do', 'you', 'know', 'what', 'it', 'is'],
     ['excuse', 'me', 'do', 'you', 'know', 'what', 'time', 'it', 'is'],
   ]
-  const wordsMapping = new WordsMapping(sentences)
+  const keyWords = ['about', 'thing', 'time']
+  const wordsMapping = new WordsMapping(sentences, keyWords)
   wordsMapping.execute()
   const map = wordsMapping.getMappedSentences()
-  const expected = [
-    [0, 8, 4, 7, 3, 2],
-    [1, 5, 0, 8, 4, 7, 6, 3, 2],
-  ]
+  const expected = {
+    sentences: [
+      [1, 10, 5, 9, 4, 3],
+      [2, 6, 1, 10, 5, 9, 8, 4, 3],
+    ],
+    keyWords: [0, 7, 8],
+  }
   expect(map).toEqual(expected)
   // immutable map test
-  map[0] = null
+  map.sentences = null
   expect(wordsMapping.getMappedSentences()).toEqual(expected)
 })
 
 test('Returns correct map to empty sentences', () => {
-  const wordsMapping = new WordsMapping()
+  const wordsMapping = new WordsMapping([], [])
   wordsMapping.execute()
-  expect(wordsMapping.getMappedSentences()).toEqual([])
+  expect(wordsMapping.getMappedSentences()).toEqual({ sentences: [], keyWords: [] })
 })
--- src/summarizer/WordsMapping.js
+++ src/summarizer/WordsMapping.js
@@ -1,31 +1,36 @@
 import { sort, binarySearch } from './util'
 
 export default class WordsMapping {
-  constructor (sentences = []) {
+  constructor (sentences = [], keyWords = []) {
     this._sentences = sentences
+    this._keyWords = keyWords
   }
 
   execute () {
     const wordsMap = this.generateMap(this._sentences)
     sort(wordsMap)
-    this._mappedSentences = this.generateMappedSentences(this._sentences, wordsMap)
+    this._mappedSentences = this.generateMappedSentences(wordsMap)
   }
 
-  generateMap (sentences) {
+  generateMap () {
     const wordsSet = new Set()
-    sentences.forEach(
+    this._sentences.forEach(
       sentence => sentence.forEach(word => wordsSet.add(word))
     )
+    this._keyWords.forEach(word => wordsSet.add(word))
     return Array.from(wordsSet)
   }
 
-  generateMappedSentences (sentences, wordsMap) {
-    return sentences.map(
-      sentence => sentence.map(word => binarySearch(word, wordsMap))
-    )
+  generateMappedSentences (wordsMap) {
+    return {
+      sentences: this._sentences.map(
+        sentence => sentence.map(word => binarySearch(word, wordsMap))
+      ),
+      keyWords: this._keyWords.map(word => binarySearch(word, wordsMap)),
+    }
   }
 
   getMappedSentences () {
-    return [...this._mappedSentences]
+    return Object.assign({}, this._mappedSentences)
   }
 }
