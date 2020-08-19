1e80d73c1ca0a58a3fedc0c787f802c02b95138d
Erick Alves <e7alves@gmail.com>
2020-08-19 20:27:40 -0300

    Stopwords and stemmer to english

--- src/summarizer/Stemmer.js
+++ src/summarizer/Stemmer.js
@@ -1,10 +1,17 @@
+import Snowball from 'snowball'
+
+const languages = {
+  en: 'English',
+}
 
 export default class Stemmer {
   constructor (lang = 'en') {
-    this.lang = lang
+    this._snowball = new Snowball(languages[lang] || languages.en)
   }
 
   getCanonical (word) {
-    return null
+    this._snowball.setCurrent(word)
+    this._snowball.stem()
+    return this._snowball.getCurrent()
   }
 }
