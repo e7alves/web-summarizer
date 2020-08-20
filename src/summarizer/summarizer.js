51db121ed52c5dfc47b7640862b42d0748b01414
Erick Alves <e7alves@gmail.com>
2020-08-20 19:54:09 -0300

    Summarizer facade class

--- src/summarizer/summarizer.js
+++ src/summarizer/summarizer.js
@@ -1,59 +1,35 @@
-/* eslint-disable */
+import { transform } from './preProcessor'
+import WordsMapping from './WordsMapping'
+import BushyPath from './BushyPath'
+import { calculateNumberOfSentencesToSummary } from './compressor'
 
-const wordsSet = new Set();
+export default class Sumarizer {
+  constructor (paragraphs, keyWords, lang = 'en') {
+    this._paragraphs = paragraphs
+    this._keyWords = keyWords
+    this._lang = lang
+  }
 
+  execute () {
+    const transformedSentences = transform(this._paragraphs)
+    const transformedKeyWords = transform([this._keyWords])[0]
+    const wordsMapping = new WordsMapping(transformedSentences, transformedKeyWords)
+    wordsMapping.execute()
+    const map = wordsMapping.getMappedSentences()
+    const busyPath = new BushyPath(map.sentences, map.keyWords)
+    busyPath.execute()
+    const rank = busyPath.getRank()
+    const rankedSentences = []
+    rank.forEach((item, idx) => {
+      rankedSentences[idx] = this._paragraphs[item.index]
+    })
+    const numberOfSentencesToSummary = calculateNumberOfSentencesToSummary(rankedSentences, 0.3)
+    this._summary = rank.slice(0, numberOfSentencesToSummary)
+      .sort((a, b) => a.index < b.index ? -1 : 1)
+      .map(item => this._paragraphs[item.index])
+  }
 
-
-
-
-const cosSimilarity = (array1, array2) => {
-  
-
-	var i, j, arrayLength1, arrayLength2, flag,
-	conj1 = [], conj2 = [], contIntersec;
-	arrayLength1 = palavras1.length;
-	for(i = 0; i < arrayLength1; i++) {
-		if (palavras1[i].canonica != "") {
-			arrayLength2 = conj1.length;
-			flag = true;
-			for(j = 0; j < arrayLength2; j++) {
-				if (palavras1[i].canonica == conj1[j]) {
-					flag = false;
-					break;
-				}
-			}
-			if (flag) {
-				conj1[arrayLength2] = palavras1[i].canonica;
-			}
-		}
-	}
-	arrayLength1 = palavras2.length;
-	for(i = 0; i < arrayLength1; i++) {
-		if (palavras2[i].canonica != "") {
-			arrayLength2 = conj2.length;
-			flag = true;
-			for(j = 0; j < arrayLength2; j++) {
-				if (palavras2[i].canonica == conj2[j]) {
-					flag = false;
-					break;
-				}
-			}
-			if (flag) {
-				conj2[arrayLength2] = palavras2[i].canonica;
-			}
-		}
-	}
-	
-	arrayLength1 = conj1.length;
-	arrayLength2 = conj2.length;
-	contIntersec = 0;
-	for(i = 0; i < arrayLength1; i++) {
-		for(j = 0; j < arrayLength2; j++) {
-			if (conj1[i] == conj2[j]) {
-				contIntersec++;
-			}
-		}
-	}
-	return contIntersec / (Math.sqrt(arrayLength1)*Math.sqrt(arrayLength2)); 
-	
+  getSummary () {
+    return this._summary
+  }
 }
